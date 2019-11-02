996ff80e8eaedaa4e7ae2ad77f381feb02992f6b
cy857213312 <35824619+cy857213312@users.noreply.github.com>
2019-11-02 23:31:02 +0800

    swagger2 更新

--- src/main/java/com/imufe/company/controller/TestController.java
+++ src/main/java/com/imufe/company/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.imufe.company.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/say")
+@Api(value = "测试Swagger2")
 public class TestController {
 
-    @RequestMapping("/he")
-    public String index(){
+    @PostMapping("/he")
+    public String index(@RequestParam("query") String query){
 
-        return "index";
+        return "index.html";
     }
 }
